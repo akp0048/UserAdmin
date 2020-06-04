d4504ae4636aafbf0d26a153827ce88ebe1001ff
akp0048 <akp0048@auburn.edu>
2020-06-04 22:09:42 +0000

    Created a simpel UserAdmin interface & connected to the AIM.

---
+++ src/main/java/edu/au/cc/gallery/tools/UserHelper.java
@@ -0,0 +1,11 @@
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagementClientBuilder;
+import com.amazonaws.services.identitymanagement.model.CreateUserRequest;
+import com.amazonaws.services.identitymanagement.model.CreateUserResult;
+
+public class UserHelper {
+
+    public UserHelper() {
+    }
+
+}
